7a32449ab5c1c8ef2896926e9e3e5f96a619947a
fhalambek <fran.halambek@gmail.com>
2017-12-19 00:07:54 +0100

    dodan weighted bot

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import random
 
+weight = [[99, -8,  8,  6,  6,  8, -8, 99],
+          [-8,-24, -4, -3, -3, -4,-24, -8],
+          [ 8, -4,  7,  4,  4,  7, -4,  8],
+          [ 6, -3,  4,  0,  0,  4, -3,  6],
+          [ 6, -3,  4,  0,  0,  4, -3,  6],
+          [ 8, -4,  7,  4,  4,  7, -4,  8],
+          [-8,-24, -4, -3, -3, -4,-24, -8],
+          [99, -8,  8,  6,  6,  8, -8, 99]]
 
 def easy(field, possible, color):
     return(random.choice(possible))
@@ -9,13 +17,14 @@ def greedy(field, possible, color, opp_color):
     cntmax = 0
     delta_x = [-1, -1,  0,  1,  1,  1,  0, -1]
     delta_y = [ 0,  1,  1,  1,  0, -1, -1, -1]
+    bestpos = possible[0]
     
     for position in range(possible.size()):
 
         counter = 0
         pos_x = possible[position][0]
         pos_y = possible[position][1]
-
+        
         for direction in range(8):
 
             dir_counter = 0
@@ -50,3 +59,21 @@ def greedy(field, possible, color, opp_color):
             bestpos = possible[position]
             
     return(bestpos)
+
+
+def weighted(field, possible, color, opp_color):
+
+    maxweight = 0
+    bestpos = (possible[0])
+    
+    for i in range(possible.size()):
+        pos_x = possible[i][0]
+        pos_y = possible[i][1]
+        if(weight[pos_x][pos_y] > maxweight):
+            maxweight = weight[pos_x][pos_y]
+            bestpos = possible[i]
+
+    return(bestpos)
+            
+
+        
